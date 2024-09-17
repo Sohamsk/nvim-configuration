bb79ffdfd73b781e9da44eed79c82a22aa8a7429
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-09-17 20:38:34 +0530

    made yank to clipboard work with linux

--- lua/soham/remaps/init.lua
+++ lua/soham/remaps/init.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "P", "\"+y")
+vim.keymap.set("v", "Y", "\"+yg_")
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
